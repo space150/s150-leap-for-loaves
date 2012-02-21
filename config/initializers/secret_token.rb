defcdf288ec7dadeff40c7c653f3ca28a8138abd
Shawn Roske <sroske@gmail.com>
2012-02-21 09:01:54 -0600

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leapforloaves::Application.config.secret_token = '175791f307399e757d7fe86a5bbaa68748b689b9e65c1ecde539b72fa513270bb023ba13efdf614a3dc4a4944156d27111b92ea61321b0e501255bd4de24f3ae'
