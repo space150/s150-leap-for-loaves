277de6989c398389d9210df7f1bb8f777c51a91b
Shawn Roske <sroske@gmail.com>
2012-02-21 17:29:32 -0600

    removing some unused views, added a simple coming soon default screen

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Leapforloaves::Application.routes.draw do
   match 'leaps' => 'leaps#create', :via => :post, :as => :new_leap
   match 'leap' => 'leaps#new', :via => :get, :as => :leap
   
-  match 'welcome' => 'welcome#index', :via => :get, :as => :step1
-  match 'step2' => 'welcome#step2', :via => :get, :as => :step2
-  match 'step3' => 'welcome#step3', :via => :get, :as => :step3
-  match 'step4' => 'welcome#step4', :via => :get, :as => :step4
+  match 'welcome' => 'welcome#index', :via => :get, :as => :welcome
   match 'about' => 'welcome#about', :via => :get, :as => :about
   
-  root :to => 'leaps#index', :via => :get
+  root :to => 'welcome#index', :via => :get
   
 end
