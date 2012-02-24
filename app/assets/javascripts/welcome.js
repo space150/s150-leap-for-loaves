34630908216db82c638f1d63ed68c2e9cb464d74
Shawn Roske <sroske@gmail.com>
2012-02-23 23:27:54 -0600

    Added a basic layout for the coming soon page

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,36 @@
+
+var welcome = {
+	then: new Date("Feb 29 2012 00:00:00"),
+	timer: null,
+    init: function() {
+    	this.timer = setTimeout(this.countdown.bind(this), 1000);
+    },
+    countdown: function () {
+    	var now = new Date();
+ 	  	var diff = new Date(this.then - now);
+ 		var seconds_left = Math.floor(diff.valueOf() / 1000);
+ 		var seconds = Math.floor(seconds_left / 1) % 60;
+ 		var minutes = Math.floor(seconds_left / 60) % 60;
+ 		var hours = Math.floor(seconds_left / 3600) % 24;
+ 		var days = Math.floor(seconds_left / 86400) % 86400;
+ 		this.updateDisplay( days, hours, minutes, seconds );
+ 		this.timer = setTimeout(this.countdown.bind(this), 1000);
+    },
+    updateDisplay: function ( days, hours, minutes, seconds ) {
+ 		$('#days').html(this.formatNumber(days));
+ 		$('#hours').html(this.formatNumber(hours));
+ 		$('#minutes').html(this.formatNumber(minutes));
+ 		$('#seconds').html(this.formatNumber(seconds));
+    },
+    formatNumber: function ( number ) {
+    	var value = number + '';
+ 		if ( value.length < 2 ) {
+ 	    	return "0" + value;
+ 		}
+ 		return value;
+ 	}
+};
+
+$(document).ready(function(){
+    welcome.init();	
+});
