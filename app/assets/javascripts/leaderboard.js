ef2af69587c8dcc40d19b8eb2c5b2b5c900f6458
Shawn Roske <sroske@gmail.com>
2012-02-25 16:45:17 -0600

    added leaderboard layout for ipad/iphone/android devices

--- app/assets/javascripts/leaderboard.js
+++ app/assets/javascripts/leaderboard.js
@@ -13,6 +13,25 @@ var leaderboard = {
 			$('#leap-again-button').show();
 		else
 			$('#leap-again-button').hide();
+
+		$('#read-more-button').click(this.handleReadMoreClick);
+    },
+    handleReadMoreClick: function () {
+    	var button = $('#read-more-button');
+    	var panel = $('#read-more-panel');
+    	if ( panel.is(':hidden') )
+    	{
+    		button.html('Close<i class="arrow"></i>');
+    		button.addClass('open');
+    		$('#read-more-panel').slideDown('slow');
+    	}
+    	else
+    	{
+    		button.html('Read More<i class="arrow"></i>');
+    		button.removeClass('open');
+			$('#read-more-panel').slideUp('slow');
+    	}	
+    	return false;
     }
 };
 
